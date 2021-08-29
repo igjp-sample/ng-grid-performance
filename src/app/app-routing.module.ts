d94274ccc4b8dd34cdb6c0abc1d22e38afaa5097
MotokiNakae <motoki@cleverdog.me>
2021-08-30 08:39:16 +0900

    add required files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ignite',
+    loadChildren: () => import('./ignite/ignite.module').then(m => m.IgniteModule)
+  },
+  {
+    path: 'material',
+    loadChildren: () => import('./material/material.module').then(m => m.MaterialModule)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FinancialService } from "./services/financial.service";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [FinancialService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
